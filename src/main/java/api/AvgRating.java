1eba97ad1f484f7db64c4346ad3d3b7606fc2c7e
unknown <m15210183955@gmail.com>
2023-10-01 14:39:25 -0400

    fix comment for AvgRating 1

--- src/main/java/api/AvgRating.java
+++ src/main/java/api/AvgRating.java
@@ -2,38 +2,37 @@ package api;
 
 public class AvgRating {
 
+    //using data class
     private final Data dataAccess;
 
+    // construct
     public AvgRating() {
         this.dataAccess = new Data();
     }
 
-    // Constructor to initialize DataAccess implementation
-
+    //Avg rating for pictures implement
     public Integer getAvgPic() {
         Integer[] pictureRatings = dataAccess.getPictureRating();
-        if(pictureRatings == null || pictureRatings.length == 0) return null;
-
+        if(pictureRatings == null || pictureRatings.length == 0)
+            return null;
         int sum = 0;
         for(int rating : pictureRatings) {
             sum += rating;
         }
-
         return sum / pictureRatings.length;
     }
 
+    //Avg rating for facts implement
     public Integer getAvgFact() {
         Integer[] factRatings = dataAccess.getFactRating();
-        if(factRatings == null || factRatings.length == 0) return null;
-
+        if(factRatings == null || factRatings.length == 0)
+            return null;
         int sum = 0;
         for(int rating : factRatings) {
             sum += rating;
         }
-
         return sum / factRatings.length;
     }
-
 
 }
 
